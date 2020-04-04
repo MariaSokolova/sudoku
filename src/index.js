0cfee86590ce56f55864b226a837c9f865b686db
Maria Sokolova <sb_mv@inbox.ru>
2020-04-04 15:19:46 +0200

    feat: implement solveSudoku

--- src/index.js
+++ src/index.js
@@ -1,3 +1,46 @@
 module.exports = function solveSudoku(matrix) {
-  // your solution
-}
+    for (let i = 0; i < 9; i++) {
+        for (let j = 0; j < 9; j++) {
+            if (matrix[i][j] === 0) {
+                for (let number = 1; number <= 9; number++) {
+                    if(checkNumber(matrix, i, j, number)) {
+                        matrix[i][j] = number;
+                        const result = solveSudoku(matrix);
+                        if(result !== false) {
+                            return result;
+                        }
+                        matrix[i][j] = 0;
+                    }
+                }
+                return false;
+            }
+        }
+    }
+    return matrix;
+};
+
+const checkNumber = function (matrix, row, column, number) {
+    for (let i = 0; i < 9; i++) {
+        const matrixRow = matrix[row];
+        if(matrixRow[i] === number) {
+            return false;
+        }
+    }
+
+    for (let i = 0; i < 9; i++) {
+        if(matrix[i][column] === number) {
+            return false;
+        }
+    }
+
+    const boxRowStart = row - row % 3;
+    const boxColStart = column - column % 3;
+    for (let i = boxRowStart; i < boxRowStart + 3; i++) {
+        for (let j = boxColStart; j < boxColStart + 3; j++) {
+            if(matrix[i][j] === number) {
+                return false;
+            }
+        }
+    }
+    return true;
+};
